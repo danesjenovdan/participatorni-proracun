cd0f780af76c234557a396bb888990f66cefd3eb
LepkoQQ <lepko.san@gmail.com>
2020-11-03 18:13:21 +0100

    move base url to /zaobljube

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const server = require('./server/server');
 
 module.exports = {
-  baseUrl: process.env.NODE_ENV === 'production' ? '/participativni-proracun/' : '/',
+  baseUrl: process.env.NODE_ENV === 'production' ? '/participativni-proracun/zaobljube/' : '/',
   pluginOptions: {
     ssr: {
       port: 8801,
