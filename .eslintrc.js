34671525f3dc7851c1cf56e388f1aa54d860eb6d
Lan Strlič <lan.strlic@gmail.com>
2020-11-11 22:34:00 +0100

    various fixes, added more images for cards,

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,45 +1,45 @@
-// module.exports = {
-//   root: true,
-//   env: {
-//     node: true,
-//   },
-//   extends: ['plugin:vue/recommended', '@vue/airbnb'],
-//   // rules: {
-//   //   'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
-//   //   // windows linebreaks when not in production environment
-//   //   "linebreak-style": ["error", process.env.NODE_ENV === 'production' ? "unix" : "windows"],
-//   //   "indent": "off",
-//   //   'import/prefer-default-export': 'off',
-//   //   'comma-dangle': [
-//   //     'error',
-//   //     {
-//   //       arrays: 'always-multiline',
-//   //       objects: 'always-multiline',
-//   //       imports: 'always-multiline',
-//   //       exports: 'always-multiline',
-//   //       functions: 'never',
-//   //     },
-//   //   ],
-//   //   'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
-//   //   'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
-//   //   'vue/html-closing-bracket-newline': [
-//   //     'error',
-//   //     {
-//   //       singleline: 'never',
-//   //       multiline: 'always',
-//   //     },
-//   //   ],
-//   //   'vue/html-closing-bracket-spacing': [
-//   //     'error',
-//   //     {
-//   //       startTag: 'never',
-//   //       endTag: 'never',
-//   //       selfClosingTag: 'always',
-//   //     },
-//   //   ],
-//   // },
-//   parserOptions: {
-//     parser: 'babel-eslint',
-//     ecmaVersion: 7,
-//   },
-// };
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: ['plugin:vue/recommended', '@vue/airbnb'],
+  rules: {
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    // windows linebreaks when not in production environment
+    "linebreak-style": ["error", process.env.NODE_ENV === 'production' ? "unix" : "windows"],
+    "indent": "off",
+    'import/prefer-default-export': 'off',
+    'comma-dangle': [
+      'error',
+      {
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'never',
+      },
+    ],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'vue/html-closing-bracket-newline': [
+      'error',
+      {
+        singleline: 'never',
+        multiline: 'always',
+      },
+    ],
+    'vue/html-closing-bracket-spacing': [
+      'error',
+      {
+        startTag: 'never',
+        endTag: 'never',
+        selfClosingTag: 'always',
+      },
+    ],
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+    ecmaVersion: 7,
+  },
+};
