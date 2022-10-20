fdc5d2558f2ace03f4cbfc08aa5ef4711f23c870
LepkoQQ <lepko.san@gmail.com>
2022-10-20 11:22:39 +0200

    temp disable eslint, change public path key

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const server = require('./server/server');
 
 module.exports = {
-  baseUrl: process.env.NODE_ENV === 'production' ? '/participativni-proracun/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/participativni-proracun/' : '/',
   pluginOptions: {
     ssr: {
       port: 8801,
