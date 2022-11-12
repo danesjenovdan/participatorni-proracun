35974688bb950654b6f5e9e2bd502dfbc04ec753
LepkoQQ <lepko.san@gmail.com>
2022-11-12 18:44:16 +0100

    generate new pp list

--- build/generate-pp-list.js
+++ build/generate-pp-list.js
@@ -1,39 +1,43 @@
 /* eslint-disable no-console */
 /* eslint-disable no-restricted-syntax */
+import axios from "axios";
 import fs from "fs-extra";
-import { slugify } from "./slugify.js";
+import { prepareNameForSlug, slugify } from "./slugify.js";
 
 const municipalities = fs.readJsonSync("data/municipalities.json");
-const dataFiles = fs.readdirSync("data/");
-const candidatesFile = dataFiles
-  .filter((f) => f.endsWith("_candidates.json"))
-  .sort((a, b) => a.localeCompare(b) * -1)[0];
-const allCandidates = fs.readJsonSync(`data/${candidatesFile}`);
-// const date = candidatesFile.split("_candidates.json")[0];
+const allCandidates = fs.readJsonSync("data/candidates.json");
 
 async function main() {
   const all = [];
 
+  const response = await axios.get(
+    "https://glas-ljudstva.si/api/municipalities/?election_id=3&question_ids=288"
+  );
+  const candidatesData = response.data.parties;
+  const answers = response.data.questions["288"].party_answers;
+
   for (const m of municipalities.dvk) {
     const id = m[0];
     const name = m[2];
-    const candidates = allCandidates.filter((o) => o["Občina"] === name);
+    const candidates = allCandidates.filter((o) => o.municipality === name);
 
     all.push({
       id,
       name,
       slug: slugify(name),
       candidates: candidates.map((o) => {
-        // FIXME: fetch data from api
-        const hasPP = Math.random() < 0.25;
-        const promisedPP = hasPP || Math.random() < 0.25;
+        const cData = candidatesData.find((c) => c.party_name === o.name);
+        let promisedPP = false;
+        if (cData) {
+          promisedPP = answers[cData.id];
+        }
 
         return {
-          name: o.Ime,
-          slug: slugify(o.Ime),
-          gender: "m", // FIXME: add data
-          proposer: o.Predlagatelj,
-          has_pp: hasPP,
+          name: o.name,
+          slug: slugify(prepareNameForSlug(o.name)),
+          gender: o.gender,
+          proposer: o.proposer,
+          has_pp: o.already_has_pp,
           promised_pp: promisedPP,
         };
       }),
