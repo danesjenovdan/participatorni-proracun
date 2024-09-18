85841476d22d14ef1f297e12f6928103dbe4a703
LepkoQQ <lepko.san@gmail.com>
2024-09-18 16:25:09 +0200

    add configuration for dockerization and k8s

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,10 @@
 const server = require('./server/server');
 
 module.exports = {
-  baseUrl: process.env.NODE_ENV === 'production' ? '/participativni-proracun/zaobljube/' : '/',
+  baseUrl: process.env.NODE_ENV === 'production' ? '/2018/zaobljube/' : '/',
   pluginOptions: {
     ssr: {
+      host: true,
       port: 8801,
       // extendServer: server,
     },
