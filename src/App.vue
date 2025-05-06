9ee8f50cafc6ec365ea5a48e93844bce9237145e
LepkoQQ <lepko.san@gmail.com>
2025-05-06 15:25:20 +0200

    add favicon

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,29 @@
 </template>
 
 <script setup>
-import { useSeoMeta } from "@unhead/vue";
+import { useHead, useSeoMeta } from "@unhead/vue";
+
+useHead({
+  link: [
+    {
+      rel: "apple-touch-icon",
+      sizes: "180x180",
+      href: "/apple-touch-icon.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      href: "/favicon-32x32.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+      href: "/favicon-16x16.png",
+    },
+  ],
+});
 
 // title: "TODO: Participativni proračun",
 // titleTemplate: "%s | My Site",
